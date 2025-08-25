9b1b2260e6fab6bd74e4db5c5875056c60b50a98
Ryan Borromeo <ryanzizarborromeo@gmail.com>
2025-08-25 10:38:21 +0800

    Fixed showing of data via inertia

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,14 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all.order(:code)
     render inertia: 'Products/Index', props: {
-      products: @products.as_json(only: [:id, :code, :name, :price])
+      products: @products.map do |p|
+        {
+          id: p.id,
+          code: p.code,
+          name: p.name,
+          price: p.price.to_f
+        }
+      end
     }
   end
 end
