54df37d5f81e1dd563ad863534305a64afd1f417
Ryan Borromeo <ryanzizarborromeo@gmail.com>
2025-08-25 13:55:22 +0800

    Added input validation to all controller actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,12 @@ class ApplicationController < ActionController::Base
   end
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  private
+
+  def record_not_found
+    redirect_to root_path, alert: "Record not found."
+  end
 end
